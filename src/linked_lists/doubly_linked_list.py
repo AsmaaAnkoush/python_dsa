e8843d5cc68ee848ceb8853ddfc7993c1ec73852
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 10:02:53 +0200

    feat: implement insert_at_tail method that adds the value to the end of doubly linked list

--- src/linked_lists/doubly_linked_list.py
+++ src/linked_lists/doubly_linked_list.py
@@ -23,3 +23,17 @@ class DoublyLinkedList:
             self.head.prev = node
             self.head = node
         self.length += 1
+    
+    def insert_at_tail(self, data):
+        '''this method adds the value to the end of doubly linked list'''
+        #Time complexity = O(1)
+        #Space complexity = O(1)
+        node: DoublyNode = DoublyNode(data)
+        if self.head is None:
+            self.head = node 
+            self.tail = node
+        else:
+            self.tail.next = node
+            node.prev = self.tail
+            self.tail = node
+        self.length += 1
