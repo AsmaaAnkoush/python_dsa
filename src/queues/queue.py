e66c282f412e18ed0303070e60d359463747485f
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 14:54:58 +0200

    feat: implement Queue constructor with head and tail pointers and length tracking

--- src/queues/queue.py
+++ src/queues/queue.py
@@ -2,3 +2,9 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+class Queue:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length: int = 0
