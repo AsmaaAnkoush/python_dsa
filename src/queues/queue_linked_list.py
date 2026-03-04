64bfd1dd9de9209bfbca80f6fa5f13f0160b1c93
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 15:34:52 +0200

    feat: implement enqueue method that adds data to the end of queue

--- src/queues/queue_linked_list.py
+++ src/queues/queue_linked_list.py
@@ -5,3 +5,10 @@ class Queue:
         self.elments: LinkedList = LinkedList()
         self.length: int = 0
     
+    def enqueue(self, data):
+        '''This method adds data to the end of queue'''
+        #Time Complexity O(n)
+        #Space complexity O(1)
+        self.elments.append_end(data)
+        self.length += 1
+
