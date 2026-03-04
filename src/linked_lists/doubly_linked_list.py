7b11bebcda52a496795817ec472f2d639938588b
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 09:41:16 +0200

    feat: implement Node class for doubly linked list

---
+++ src/linked_lists/doubly_linked_list.py
@@ -0,0 +1,6 @@
+class DoublyNode:
+    def __init__(self, value):
+        '''this function is constructor to the class Node'''
+        self.value = value
+        self.next = None
+        self.prev = None
