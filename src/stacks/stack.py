55a12446ba8200fc0f8073fab28748764ba23790
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 17:24:45 +0200

    feat: implement push method that pushes a new element onto the stack

--- src/stacks/stack.py
+++ src/stacks/stack.py
@@ -7,3 +7,12 @@ class Stack:
     def __init__(self):
         self.top = None
         self.length: int = 0
+    
+    def push(self, data):
+        '''This method pushes a new element onto the stack'''
+        #Time Complexity is O(1)
+        #Space complexity is o(1)
+        node: Node = Node (data)
+        node.next = self.top
+        self.top = node
+        self.length += 1
