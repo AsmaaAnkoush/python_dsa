6558a898ccef883d24478f3e74a0be28400cf21f
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 15:43:05 +0200

    test: add test_dequeue unit test and make it pass

--- tests/test_queues/test_queue_linked_list.py
+++ tests/test_queues/test_queue_linked_list.py
@@ -3,8 +3,20 @@ from src.queues.queue_linked_list import Queue
 
 def test_enqueue():
     queue: Queue = Queue()
-    assert queue.length == 0
+    assert queue.elments.length == 0
     queue.enqueue(10)
     queue.enqueue(20)
     queue.enqueue(30)
-    assert queue.length == 3
+    assert queue.elments.length == 3
+
+def test_dequeue():
+    queue: Queue = Queue()
+    empty_queue: Queue = Queue()
+    queue.enqueue(10)
+    queue.enqueue(20)
+    queue.enqueue(30)
+    assert queue.elments.length == 3
+    queue.dequeue()
+    assert queue.elments.length == 2
+    with pytest.raises(IndexError):
+        empty_queue.dequeue()
