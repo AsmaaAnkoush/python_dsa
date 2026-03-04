46bc4f2fcc5527c49a9584cfaf505cfa4b75a5c4
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 09:20:03 +0200

    test: add test_map unit test and  make it pass

--- tests/test_linked_lists/test_linked_list.py
+++ tests/test_linked_lists/test_linked_list.py
@@ -82,3 +82,15 @@ def test_for_each():
     assert ll.contains(6) is True
     assert ll.contains(1) is False
     assert ll.contains(2) is True
+
+def test_map():
+    ll = LinkedList()
+    ll.append_end(1)
+    ll.append_end(2)
+    ll.append_end(3)
+
+    new_list = ll.map(lambda x: x * 2)
+
+    assert new_list.index_of(2) is 0
+    assert new_list.index_of(4) is 1
+    assert new_list.index_of(6) is 2
