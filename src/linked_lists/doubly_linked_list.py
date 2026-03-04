576e627500b1ea88d94c2cf7afb4eca2c353e669
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 09:59:07 +0200

    feat: implement insert_at_head method that adds a value to the beginning of the doubly linked list

--- src/linked_lists/doubly_linked_list.py
+++ src/linked_lists/doubly_linked_list.py
@@ -9,3 +9,17 @@ class DoublyLinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+    
+    def insert_at_head(self,data):
+        '''This method adds a value to the beginning of the doubly linked list'''
+        #Time complexity = O(1)
+        #Space complexity = O(1)
+        node: DoublyNode = DoublyNode(data)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        self.length += 1
