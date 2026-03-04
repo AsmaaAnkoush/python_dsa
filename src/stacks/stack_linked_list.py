d409e83a989e3764c94fa386c99a9e52f17a58ae
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 19:38:33 +0200

    feat: implement Stack constructor with linked list

---
+++ src/stacks/stack_linked_list.py
@@ -0,0 +1,6 @@
+from ..linked_lists.linked_list import LinkedList
+
+class Stack:
+    def __init__(self):
+        '''this function is constructor to the class Stack'''
+        self.elments: LinkedList = LinkedList()
