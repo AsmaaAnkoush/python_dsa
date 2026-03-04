eb409a15cf7f2c40c145305579f8e6877122cab4
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 10:45:59 +0200

    feat: implement insert method that inserts a value at the end of the circular linked list

--- src/linked_lists/circular_linked_list.py
+++ src/linked_lists/circular_linked_list.py
@@ -7,3 +7,21 @@ class CircularLinkedList:
     def __init__(self):
         self.head = None
         self.length = 0
+
+    def insert(self, data):
+        '''This method inserts a value at the end of the circular linked list'''
+        #Time Complexity = O(n)
+        #Space Complexity = O(1)
+        node: CircularNode = CircularNode(data)
+
+        if self.head == None:
+            self.head = node
+            node.next = self.head
+            self.length += 1
+        else:
+            current: CircularNode = self.head
+            while current.next != self.head:
+                current = current.next
+            current.next = node
+            node.next = self.head
+            self.length += 1
