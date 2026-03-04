e83c654d524a6d815efae1faf604b43749ba0e83
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 15:15:32 +0200

    feat: implement enqueue methos that adds data to the end of queue

--- src/queues/queue_list.py
+++ src/queues/queue_list.py
@@ -2,3 +2,10 @@ class Queue:
     def __init__(self):
         self.elements = []
         self.length = 0
+    
+    def enqueue(self, data):
+        '''This method adds data to the end of queue'''
+        #Time Complexity is O(1), o(n) if list resized
+        #Space complexity is o(1), o(n) if list resized
+        self.elements.append(data)
+        self.length += 1
