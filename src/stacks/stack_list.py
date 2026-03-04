6012508c3b312dc4f7a97a374938c6854c4d9e17
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 19:18:38 +0200

    feat: implement pop method that pops the top element from the stack, and returns the removed value

--- src/stacks/stack_list.py
+++ src/stacks/stack_list.py
@@ -9,3 +9,15 @@ class Stack:
         #Space complexity is o(1),, o(n) if list resized
         self.elements.append(data)
         self.length += 1
+    
+    def pop(self):
+        '''This method pops the top element from the stack, and returns the removed value'''
+        #Time Complexity is O(1).
+        #Space complexity is o(1).
+        if self.elements == []:
+            raise IndexError ("The Stack is Empty")
+        else:
+            poped_element = self.elements[-1]
+            self.elements.remove(self.elements[-1])
+            self.length -= 1
+            return poped_element
