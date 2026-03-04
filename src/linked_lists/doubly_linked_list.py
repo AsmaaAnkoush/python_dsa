de40fa4ffa6aa507eee44d4c38d5ac7b92c0466e
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 09:43:57 +0200

    feat: implement DoublyLinkedList constructor with head and tail pointers and length tracking

--- src/linked_lists/doubly_linked_list.py
+++ src/linked_lists/doubly_linked_list.py
@@ -3,3 +3,9 @@ class DoublyNode:
         self.value = value
         self.next = None
         self.prev = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
