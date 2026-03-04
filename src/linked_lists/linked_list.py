3b456e0ecc8f0beb88c11fa1e64b838f3a5c8e0e
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 08:54:47 +0200

    feat: implement append end method that add the value to the end of linked list

--- src/linked_lists/linked_list.py
+++ src/linked_lists/linked_list.py
@@ -9,7 +9,7 @@ class LinkedList:
         self.length = 0
     
     def add_first(self, value):
-        '''this function add the value to the first of linked list'''
+        '''this method add the value to the first of linked list'''
         #Time complexity = O(1)
         #Space complexity = O(1)
         node: Node = Node(value)
@@ -18,4 +18,18 @@ class LinkedList:
         else:
             node.next = self.head
             self.head = node
+        self.length += 1
+
+    def append_end(self, value):
+        '''this method add the value to the end of linked list'''
+        #Time complexity = O(n)
+        #Space complexity = O(1)
+        node: Node = Node(value)
+        if self.head is None:
+            self.head = node 
+        else:
+            current: Node = self.head
+            while current.next:
+                current = current.next
+            current.next = node
         self.length += 1
