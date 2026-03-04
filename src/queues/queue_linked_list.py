da39eee9ae6d2c569df0001047d8869eb3a3eb94
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 15:42:44 +0200

    feat: implement dequeue method that removes a node from the front of the queue.

--- src/queues/queue_linked_list.py
+++ src/queues/queue_linked_list.py
@@ -3,12 +3,17 @@ from ..linked_lists.linked_list import LinkedList
 class Queue:
     def __init__(self):
         self.elments: LinkedList = LinkedList()
-        self.length: int = 0
     
     def enqueue(self, data):
         '''This method adds data to the end of queue'''
         #Time Complexity O(n)
         #Space complexity O(1)
         self.elments.append_end(data)
-        self.length += 1
+    
+    def dequeue(self):
+        '''This method removes a node from the front of the queue'''
+        #Time Complexity O(n)
+        #Space complexity O(1)
+        return self.elments.remove_at(0)
+    
 
