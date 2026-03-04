fc6522be24e27d1109c99d242211868aaafa84ac
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 10:51:52 +0200

    feat: implement delete method that deletes a node from the circular linked list and returns True if the deletion is successful, or False otherwise

--- src/linked_lists/circular_linked_list.py
+++ src/linked_lists/circular_linked_list.py
@@ -25,3 +25,36 @@ class CircularLinkedList:
             current.next = node
             node.next = self.head
             self.length += 1
+
+    def delete(self, data) -> bool:
+        '''This method deletes a node from the circular linked list and returns True if the deletion is successful, or False otherwise'''
+        #Time Complexity = O(n)
+        #Space Complexity = O(1)
+        if self.head == None:
+            return False
+        current: CircularNode = self.head
+        prev = None
+        # if the the list contain one element and the data in it
+        if self.head.next == self.head and self.head.data == data:
+            self.head = None
+            self.length -= 1
+            return True
+        # if the data in the head 
+        if self.head.data == data:
+            while current.next != self.head:
+                current = current.next
+            current.next = self.head.next
+            self.head = self.head.next
+            self.length -= 1
+            return True
+        # if the data not the head 
+        prev = self.head
+        current = self.head.next 
+        while current != self.head:
+            if current.data == data:
+                prev.next = current.next
+                self.length -= 1
+                return True
+            prev = current
+            current = current.next
+        return False
