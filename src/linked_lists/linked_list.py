d6a9e5f6e4b73275b2a9b5f9c1a1020ddde21bef
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 08:51:52 +0200

    feat: implement add first to the linked list

--- src/linked_lists/linked_list.py
+++ src/linked_lists/linked_list.py
@@ -7,3 +7,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
         self.length = 0
+    
+    def add_first(self, value):
+        '''this function add the value to the first of linked list'''
+        #Time complexity = O(1)
+        #Space complexity = O(1)
+        node: Node = Node(value)
+        if self.head is None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.length += 1
