a9d4143a0fb8e63e4564db0c812c158ccf0e5b38
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-03-04 17:21:41 +0200

    feat: implement Stack constructor with top pointer and length tracking

--- src/stacks/stack.py
+++ src/stacks/stack.py
@@ -2,3 +2,8 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.length: int = 0
