376cddfab20a88466c36baefe462d11c1fb142c1
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-01 09:38:24 +0300

    feat: implement insert method thats inserts the node in true position of tree

--- src/trees/binary_search_tree.py
+++ src/trees/binary_search_tree.py
@@ -7,3 +7,23 @@ class Node:
 class BST:
     def __init__(self):
         self.root = None
+
+    def insert(self, value):
+        if not self.root:
+            self.root = Node(value)
+        else:
+            self.insert_at_true_position(self.root, value)
+    
+    def insert_at_true_position(self, current: Node, value):
+        if value < current.value:
+            # insert left 
+            if current.left:
+                self.insert_at_true_position(current.left, value)
+            else:
+                current.left = Node(value)
+        else:
+            # insert right
+            if current.right:
+                self.insert_at_true_position (current.rigth, value)
+            else:
+                current.rigth = Node(value)
