5aba1122002d2d55f6aebde727bfdf5af2ab7980
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-02 10:48:03 +0300

    test: add test_remove unit test and make it pass

--- tests/test_trees/test_binary_search_tree.py
+++ tests/test_trees/test_binary_search_tree.py
@@ -10,3 +10,32 @@ def test_insert():
     assert bst.root.right.value == 20
     assert bst.root.left.value == 5
     assert bst.root.left.left.value == 0
+
+def test_remove():
+    bst: BST = BST()
+    bst.insert(10)
+    bst.insert(20)
+    bst.insert(5)
+    bst.insert(0)
+    bst.insert(7)
+    bst.insert(25)
+    bst.insert(18)
+    bst.insert(6)
+
+    assert bst.root.value == 10
+    assert bst.root.right.value == 20
+    assert bst.root.right.right.value == 25
+    assert bst.root.right.left.value == 18
+    assert bst.root.left.value == 5
+    assert bst.root.left.right.value == 7
+    assert bst.root.left.right.left.value == 6
+    assert bst.root.left.left.value == 0
+    bst.remove(bst.root, 5)
+    assert bst.root.value == 10
+    assert bst.root.right.value == 20
+    assert bst.root.right.right.value == 25
+    assert bst.root.right.left.value == 18
+    assert bst.root.left.value == 6
+    assert bst.root.left.right.value == 7
+    assert bst.root.left.left.value == 0
+    
