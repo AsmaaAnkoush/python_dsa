11bf5b50f52b048d041515a6ad5a8da85013513f
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-04 17:50:52 +0300

    feat: implement Node class and AVL class to AVL tree

---
+++ src/trees/avl_tree.py
@@ -0,0 +1,10 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+        self.height = 1
+
+class AVL:
+    def __init__(self):
+        self.root = None
