ab300e6fe424ee96ce16e459c4224987ebcd3a54
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-05 10:13:08 +0300

    test: add test_insert_avl unit test and make it pass

---
+++ tests/test_trees/test_avl_tree.py
@@ -0,0 +1,16 @@
+from src.trees.avl_tree import AVL
+
+def test_insert_avl():
+    avl = AVL()
+    avl.root = avl.insert(avl.root, 10)
+    avl.root = avl.insert(avl.root, 20)
+    avl.root = avl.insert(avl.root, 30)
+    avl.root = avl.insert(avl.root, 40)
+    avl.root = avl.insert(avl.root, 12)
+    assert avl.root.value == 20 
+    assert avl.root.right.value == 30
+    assert avl.root.left.value == 10
+    assert avl.root.right.right.value == 40 
+    assert avl.root.left.right.value == 12
+
+
