a3f9f97073ad540db5854b030da854c36e242ae8
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-05 10:42:35 +0300

    test: add test_remove unit test and make it pass

--- tests/test_trees/test_avl_tree.py
+++ tests/test_trees/test_avl_tree.py
@@ -13,4 +13,19 @@ def test_insert_avl():
     assert avl.root.right.right.value == 40 
     assert avl.root.left.right.value == 12
 
+def test_remove():
+    avl = AVL()
+    avl.root = avl.insert(avl.root, 10)
+    avl.root = avl.insert(avl.root, 20)
+    avl.root = avl.insert(avl.root, 30)
+    avl.root = avl.insert(avl.root, 40)
+    avl.root = avl.insert(avl.root, 12)
+    avl.root = avl.insert(avl.root, 25)
+
+    avl.root = avl.remove(avl.root, 30)
+    avl.root = avl.remove(avl.root, 10)
+    assert avl.root.value == 20 
+    assert avl.root.right.value == 40
+    assert avl.root.left.value == 12
+    assert avl.root.right.left.value == 25
 
