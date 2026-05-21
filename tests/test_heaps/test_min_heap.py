ac0aae3f606d2ebc7fd673a1280bf9725a45d9d4
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-21 08:22:45 +0300

    test: implement test_index_of and make it pass

--- tests/test_heaps/test_min_heap.py
+++ tests/test_heaps/test_min_heap.py
@@ -21,3 +21,14 @@ def test_search():
 
     assert heap.search(12) is True
     assert heap.search(10) is False
+
+def test_index_of():
+    heap = MinHeap()
+    heap.insert(34)
+    heap.insert(8)
+    heap.insert(12)
+    heap.insert(4)
+
+    assert heap.index_of(8) == 1
+    assert heap.index_of(10) == -1
+    assert heap.index_of(4) == 0
