917ecdf3b909478c16b58e7624a226dadd41c447
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-21 08:15:33 +0300

    test: implement test_search and make it pass

--- tests/test_heaps/test_min_heap.py
+++ tests/test_heaps/test_min_heap.py
@@ -12,3 +12,12 @@ def test_insert():
     assert heap.heap[2] == 12
     assert heap.heap[3] == 34
 
+def test_search():
+    heap = MinHeap()
+    heap.insert(34)
+    heap.insert(8)
+    heap.insert(12)
+    heap.insert(4)
+
+    assert heap.search(12) is True
+    assert heap.search(10) is False
