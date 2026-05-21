8479eb2a2be9523c99805a5625695635181fa0a1
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-21 09:49:26 +0300

    test: add test_delete unit test and make it pass

--- tests/test_heaps/test_min_heap.py
+++ tests/test_heaps/test_min_heap.py
@@ -32,3 +32,23 @@ def test_index_of():
     assert heap.index_of(8) == 1
     assert heap.index_of(10) == -1
     assert heap.index_of(4) == 0
+
+def test_delete():
+    heap = MinHeap()
+    heap.insert(34)
+    heap.insert(8)
+    heap.insert(12)
+    heap.insert(6)
+    heap.insert(9)
+    heap.insert(11)
+    heap.insert(1)
+
+    heap.delete(8)
+    assert heap.index_of(1) == 0
+    assert heap.index_of(9) == 1
+    assert heap.index_of(6) == 2
+    assert heap.index_of(34) == 3
+    assert heap.index_of(11) == 4
+    assert heap.index_of(12) == 5
+    assert heap.index_of(8) == -1
+
