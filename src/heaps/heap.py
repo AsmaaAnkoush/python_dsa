43f178f98e4018cdf5f4d2631b58c8efe68e8af8
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-22 16:43:11 +0300

    feat: implement insert method that insert to heap

---
+++ src/heaps/heap.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class MinHeap:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        node = Node(value)
+        if self.root is None:
+            self.root = node
+            return
+        self.inser_node(self.root, node)
+        self.compare_with_parent(node)
+        
+    def insert_node(self, current, node):
+        if current.left is None:
+            current.left = node
+            node.parent = current
+            return
+        if current.right is None:
+            current.right = node
+            node.parent = current
+            return
+        self.insert_node(current.left, node)
+
+    def compare_with_parent(self, node):
+        while node.parent and node.parent.value > node.value:
+            # swap
+            node.value, node.parent.value = (node.parent.value, node.value)
+            node = node.parent
+    
+    def print_heap(self, node):
+        if node is None:
+            return 
+        print(node.value)
+        self.print_heap(node.left)
+        self.print_heap(node.right)
+
+
+        
