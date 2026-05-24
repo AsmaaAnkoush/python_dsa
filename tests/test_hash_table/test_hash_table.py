965c5dda2799bf5dae43c271edc1cd14300bb402
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-24 12:32:59 +0300

    test: add test_remove unit test and make it pass

--- tests/test_hash_table/test_hash_table.py
+++ tests/test_hash_table/test_hash_table.py
@@ -17,4 +17,16 @@ def test_get():
     
     assert hash_table.get("age") == 25
     assert hash_table.get("name") == "Asmaa"
+    assert hash_table.get("weight") == "key not found"
+
+def test_remove():
+    hash_table: HashTable = HashTable()
+    hash_table.put("name", "Asmaa")
+    hash_table.put("age", 25)
+    hash_table.put("university", "Birzeit")
+    assert hash_table.get("age") == 25
+    assert hash_table.size == 3
+    hash_table.remove("name")
+    assert hash_table.get("name") == "key not found"
+    assert hash_table.size == 2
 
