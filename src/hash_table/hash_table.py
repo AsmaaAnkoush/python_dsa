cb6779e70f7a338b53be76a805df16b79e19b26e
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-24 12:01:45 +0300

    feat: implement put method that insert key, value in the hashTable

--- src/hash_table/hash_table.py
+++ src/hash_table/hash_table.py
@@ -1,14 +1,57 @@
-from typing import Optional
 class Node:
     def __init__(self, key, value):
         self.key = key
         self.value = value
-        self.next = Optional[Node] = None
+        self.next = None
     
 class HashTable:
-    def __init__(self, capacity):
+    def __init__(self, capacity = 16):
         self.capacity = capacity
         self.size = 0
         self.load_factore_threshold = 0.75
         self.array = [None] * capacity
+
+    def put (self, key, value):
+        '''This function will insert key, value to hash table'''
+        index_of_key = self.hash_key(key)
+        # if the key already exists
+        current: Node = self.array[index_of_key]
+        while current:
+            if current.key == key:
+                current.value = value
+                return
+            current = current.next
+        
+        # insert node and treat with collision using separate chaining
+        if self.array[index_of_key] is None:
+            new_node: Node = Node(key, value)
+            self.array[index_of_key] = new_node
+            self.size += 1
+        else:
+            new_node: Node = Node(key, value)
+            new_node.next = self.array[index_of_key]
+            self.array[index_of_key] = new_node
+            self.size += 1
+        
+        # check if resize_nedded
+        if self.size / self.capacity > self.load_factore_threshold:
+            self.resize()
+
+    def hash_key(self, key) -> int:
+        '''This function returns the index that the key should insert in it'''
+        return hash(key) % self.capacity
     
+    def resize(self):
+        '''This function resize the hash table if the size/capacity exceed the load factore thresold'''
+        old_array = self.array
+        self.capacity *= 2
+        self.array = [None] * self.capacity
+        old_size = self.size
+        self.size = 0
+
+        for i in old_array:
+            current: Node = i
+            while current:
+                self.put(current.key, current.value)
+                current = current.next
+        print(f"the old size {old_size} and the new size {self.size} and must be equals")
