c5cce6469d8a90e6ed0dc7845204b1a6a57b2060
AsmaaAnkoush <1180411@student.birzeit.edu>
2026-05-24 12:17:30 +0300

    test: add test_get unit test and make it pass

--- tests/test_hash_table/test_hash_table.py
+++ tests/test_hash_table/test_hash_table.py
@@ -1,4 +1,5 @@
 from src.hash_table.hash_table import Node, HashTable
+
 def test_put():
     hash_table: HashTable = HashTable()
     hash_table.put("name", "Asmaa")
@@ -6,4 +7,14 @@ def test_put():
     hash_table.put("university", "Birzeit")
 
     assert hash_table.size == 3
+
+def test_get():
+    hash_table: HashTable = HashTable()
+    hash_table.put("name", "Asmaa")
+    hash_table.put("age", 25)
+    hash_table.put("university", "Birzeit")
+    
+    
+    assert hash_table.get("age") == 25
+    assert hash_table.get("name") == "Asmaa"
 
